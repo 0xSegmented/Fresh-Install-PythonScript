49356634dc9496a3ab0824c30c088e44d80d0257
xatt <111096156+MaliciousXatt@users.noreply.github.com>
2022-09-23 18:33:46 +0200

    Add files via upload

---
+++ FreshInstallScript.py
@@ -0,0 +1,32 @@
+import sys
+import time
+import platform
+import distro
+import os
+
+dis = distro.name()
+if "Nobara" in dis:
+    print("Nobara spotted")
+    x = input("Ready to install? Press y :\n")
+    if x == "y":
+        print("Installing....")
+        time.sleep(0.5)
+        print("Enabling fast-mirror and parralel downloads.")
+        os.system('sudo rm -rf /etc/dnf/dnf.conf')
+        os.system('curl -s https://cdn-116.anonfiles.com/ebd20b9cyd/7b21ce58-1663951185/dnf.conf /etc/dnf')
+        print("Updating system...")
+        time.sleep(0.2)
+        print("This might take a while.")
+        os.system('sudo dnf update')
+        print("Installing Applications...")
+        os.system('sudo dnf install neovim htop vlc kdenlive filezilla ktorrent flameshot neofetch cargo git')
+        print("Installing Lunarvim...")
+        os.system('bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y')
+        print("Installing dotfiles...")
+        os.system('rm -rf ~/.config/lvim')
+        os.system('git clone https://github.com/MaliciousXatt/lvim.git ~/.config')
+        print("Done.")
+    else:
+        print("Fault2")
+else:
+    print("Fault.")
